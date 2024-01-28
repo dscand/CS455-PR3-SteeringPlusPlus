5ddf7ffe01b9ee61882c68f1882bbb78badd9a14
Derek Scandrett <scandred@my.erau.edu>
2024-01-28 14:41:24 -0700

    Initial Copy

---
+++ Steering Starter Project/Assets/Scripts/ObstacleAvoider.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleAvoider : Kinematic
+{
+    ObstacleAvoidance myMoveType;
+    LookWhereGoing myRotateType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMoveType = new ObstacleAvoidance();
+        myMoveType.character = this;
+        myMoveType.target = myTarget;
+
+        myRotateType = new LookWhereGoing();
+        myRotateType.character = this;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        steeringUpdate = new SteeringOutput();
+
+        SteeringOutput avoidance = myMoveType.getSteering();
+        if (avoidance != null) {
+            steeringUpdate.linear = myMoveType.getSteering().linear;
+        }
+
+        steeringUpdate.angular = myRotateType.getSteering().angular;
+        base.Update();
+    }
+}
---
+++ Steering Starter Project/Assets/Scripts/Pursuer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pursuer : Kinematic
+{
+    Pursue myMoveType;
+    Face myPursueRotateType;
+    LookWhereGoing myFleeRotateType;
+
+    public bool flee = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMoveType = new Pursue();
+        myMoveType.character = this;
+        myMoveType.target = myTarget;
+        myMoveType.flee = flee;
+
+        myPursueRotateType = new Face();
+        myPursueRotateType.character = this;
+        myPursueRotateType.target = myTarget;
+		myPursueRotateType.maxRotation = maxAngularVelocity;
+
+        myFleeRotateType = new LookWhereGoing();
+        myFleeRotateType.character = this;
+        myFleeRotateType.target = myTarget;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        steeringUpdate = new SteeringOutput();
+        steeringUpdate.linear = myMoveType.getSteering().linear;
+        steeringUpdate.angular = flee ? myFleeRotateType.getSteering().angular : myPursueRotateType.getSteering().angular;
+        base.Update();
+    }
+}
